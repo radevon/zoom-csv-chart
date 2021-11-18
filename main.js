6906a2e58f3598ffdaf2463b45cbf0cc4ec8fcc7
Emeljanov <radevon@mail.ru>
2021-11-18 15:58:33 +0300

    rebuild with electron

---
+++ main.js
@@ -0,0 +1,21 @@
+const {BrowserWindow, app} = require('electron')
+
+// Дождаться полной инициализации Electron
+// Только после этого возможно создавать окна
+app.whenReady().then(() => {
+    // Создаёт новое окно браузера
+    const win = new BrowserWindow({
+        show:false
+    })
+
+    win.setMenuBarVisibility(false);
+    win.maximize();
+    // Загружаем в окне веб-содержимое
+    win.loadFile('index.html')
+
+    win.once('ready-to-show', win.show)
+})
+
+// Завершить работу приложения 
+// если пользователь закрыл все окна программы
+app.on('window-all-closed', app.quit)
